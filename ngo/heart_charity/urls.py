5eb88f1fd9969dbd1c0a8cee9c69db77389ff5d0
Varshu99 <72430323+Varshu99@users.noreply.github.com>
2025-04-26 06:16:45 +0000

    first

---
+++ ngo/heart_charity/urls.py
@@ -0,0 +1,36 @@
+from django.urls import path
+from . import views
+from heart_charity import views
+
+
+
+urlpatterns = [
+
+    path("", views.home, name="home"),  # Home page
+    # path("issue_item/", views.issue_item, name="issue_item"),  # Book issue page
+    # path("signin/", views.login_user, name="signin"),  # Login page
+    # path("register", views.register, name="register"),  # Registration page
+    # path("logout", views.logout_view, name="logout"),  # Logout page
+    # path("return_item", views.return_item, name="return_item"),  # Return item page
+    
+    # path("history", views.history, name="history"),  # History page for issued book
+    # path(
+    #     "request_password_reset/",
+    #     views.request_password_reset,
+    #     name="request_password_reset",
+    # ),
+    # path("reset_password/<uname>/", views.reset_password, name="reset_password"),
+    # path('searchproduct/',views.searchproduct,name='searchproduct'),
+    # path('searchproductstudy/',views.searchproductstudy,name='searchproductstudy'),
+    # path('searchproductreturn/',views.searchproductreturn,name='searchproductreturn'),
+    # path('submit/', views.submit_return_date, name='submit_return_date'),
+    # path("contact_view/", views.contact_view, name="contact_view"),
+    # path('study-materials/', views.study_material_list, name='study_material_list'),
+    # path('readers/', views.readers, name='readers'),
+    # path('searchproductreaders/', views.searchproductreaders, name='searchproductreaders'),
+    # path('payment_reader/<int:reader_id>/', views.payment_reader, name='payment_reader'),
+    # path('payment',views.payment,name='payment'),
+    # path('expected_book/',views.expected_book,name='expected_book'),
+    # path('del/<str:item_id>', views.remove, name="del"),
+    # path('admin/', admin.site.urls),
+] 
